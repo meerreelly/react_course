64286437221b9eb033cabb118390de8fd2d24c00
merely <128153965+meerreelly@users.noreply.github.com>
2025-09-16 16:36:52 +0300

    lab2

---
+++ lab2/src/components/TypographyComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type TypographyProps = {
+    variant: React.ElementType;
+    children: React.ReactNode;
+    style?: React.CSSProperties;
+    className?: string;
+};
+
+const Typography  = ({ variant, children, ...props }: TypographyProps) => {
+    const Tag: React.ElementType = variant || 'p';
+    return (
+        <Tag {...props}>{children}</Tag>
+    );
+}
+
+export default Typography;
---
+++ lab2/src/types/User.ts
@@ -0,0 +1,7 @@
+type User = {
+    name: string;
+    role: string;
+    avatarUrl: string;
+}
+
+export type { User };

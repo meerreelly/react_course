b1bd5dc8e7c4014b6bbfa062d046d1a70d38eaa0
merely <128153965+meerreelly@users.noreply.github.com>
2025-09-17 13:15:27 +0300

    Refactor Profile component and update styles
    Moved Profile component and its styles to a dedicated Profile directory for better organization. Updated imports in App.tsx to reflect new paths. Adjusted light theme colors in index.css and removed Profile-related styles from global CSS, placing them in Profile/Profile.css.

--- lab2/src/App.tsx
+++ lab2/src/App.tsx
@@ -1,42 +1,50 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import type { User } from './types/User';
-import Profile from './components/Profile';
-import Typography from './components/TypographyComponent';
-
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import Profile from "./components/Profile/Profile";
+import Typography from "./components/TypographyComponent";
+import type { User } from "./types/User";
+import "./components/Profile/Profile.css";
 
 function App() {
   const [users, setUsers] = useState([] as User[]);
 
   const createUser = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
     const usersData: User[] = data.map((user: any) => ({
       name: user.name,
       role: user.company.bs,
-      avatarUrl: `https://ui-avatars.com/api/?name=${user.name}&size=250`
+      avatarUrl: `https://ui-avatars.com/api/?name=${user.name}&size=250`,
     }));
     setUsers(usersData);
-  }
+  };
 
   useEffect(() => {
     createUser();
   }, []);
 
   return (
-        <Typography variant={"div"} style={{display: 'grid',  gap: '20px', marginTop: '20px', justifyItems: 'center', gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))'}}>
-          {users.map((user, index) => (
-            <Profile
-              name={user.name}
-              role={user.role}
-              avatarUrl={user.avatarUrl}
-              className="profile"
-              key={index}
-            />
-          ))}
-      </Typography>
-  )
+    <Typography
+      variant={"div"}
+      style={{
+        display: "grid",
+        gap: "20px",
+        marginTop: "20px",
+        justifyItems: "center",
+        gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+      }}
+    >
+      {users.map((user, index) => (
+        <Profile
+          name={user.name}
+          role={user.role}
+          avatarUrl={user.avatarUrl}
+          className="profile"
+          key={index}
+        />
+      ))}
+    </Typography>
+  );
 }
 
-export default App
+export default App;
--- lab2/src/components/TypographyComponent.tsx
+++ lab2/src/components/TypographyComponent.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 
 type TypographyProps = {
-    variant: React.ElementType;
-    children: React.ReactNode;
-    style?: React.CSSProperties;
-    className?: string;
+  variant: React.ElementType;
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+  className?: string;
 };
 
-const Typography  = ({ variant, children, ...props }: TypographyProps) => {
-    const Tag: React.ElementType = variant || 'p';
-    return (
-        <Tag {...props}>{children}</Tag>
-    );
-}
+const Typography = ({ variant, children, ...props }: TypographyProps) => {
+  const Tag: React.ElementType = variant || "p";
+  return <Tag {...props}>{children}</Tag>;
+};
 
 export default Typography;
